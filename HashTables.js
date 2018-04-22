713243cc1a751cbb63d19e3f6e06acc211ee43aa
mozolevsky <alf.rulit@gmail.com>
2018-04-22 16:55:26 +0300

    Hash tables: get and getAll methods

--- HashTables.js
+++ HashTables.js
@@ -20,11 +20,17 @@ class HashTable {
 
         if (!this.table[index]) {
             this.table[index] = node;
+        } else if (this.table[index].key === node.key) {
+            this.table[index].value = node.value;
         } else {
-            console.log(`collision`);
             let currentNode = this.table[index];
 
             while (currentNode.next) {
+                if (currentNode.key === node.key) {
+                    currentNode.value = node.value;
+                    return;
+                }
+
                 currentNode = currentNode.next;
             }
 
@@ -32,6 +38,41 @@ class HashTable {
         }
     }
 
+    get(key) {
+        let index = this.hash(key);
+
+        if (!this.table[index]) {
+            return null;
+        } else {
+            let currentNode = this.table[index];
+
+            while(currentNode) {
+                if (currentNode.key === key) {
+                    return currentNode.value;
+                }
+
+                currentNode = currentNode.next;
+            }
+
+            return null;
+        }
+    }
+
+    getAllNodes() {
+        let results= [];
+
+        this.table.forEach(node => {
+            if (node) {
+                let currentNode = node;
+                while (currentNode) {
+                    results.push(currentNode);
+                    currentNode = currentNode.next;
+                }
+            }
+        });
+
+        return results;
+    }
 }
 
 class Node {
@@ -42,14 +83,17 @@ class Node {
     }
 }
 
-let ht1 = new HashTable(5); 
+let ht1 = new HashTable(20); 
 
-let node1 = new Node(`max`, `max@mail.ru`);
+let node1 = new Node(`maximilian`, `max@mail.ru`);
 let node2 = new Node(`alex`, `alex@mail.ru`);
-let node3= new Node(`alexander`, `alex@mail.ru`);
+let node3= new Node(`alex`, `alex@gmail.com`);
+let node4= new Node(`max`, `max@rambler.ru`);
 
 ht1.insert(node1);
 ht1.insert(node2);
 ht1.insert(node3);
+ht1.insert(node4);
 
 console.log(ht1.table);
+console.log(ht1.getAllNodes());
